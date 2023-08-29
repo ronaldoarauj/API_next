60f57959f03e8910d9bea2ed71f8526ffd1aac67
ronaldo araujo <ronaldoaraujo@jet.com.br>
2023-08-29 20:21:50 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,28 @@
-import { useState } from "react";
+// import { useState } from "react";
 function Home(){
     return (
     <div>
         <h1>Home</h1>
-        <Contador/>
+        {/* <Contador/> */}
     </div>
     )
     
 }
 
 
-function Contador() {
-    const [contador, setContador] = useState(1);
+// function Contador() {
+//     const [contador, setContador] = useState(1);
 
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
+//     function adicionarContador(){
+//         setContador(contador + 1);
+//     }
 
-    return (
-    <div>
-        <div>{contador}</div>
-        <button onClick={adicionarContador}>Adiciona</button>
-    </div>
-    )
-}
+//     return (
+//     <div>
+//         <div>{contador}</div>
+//         <button onClick={adicionarContador}>Adiciona</button>
+//     </div>
+//     )
+// }
 
 export default Home
