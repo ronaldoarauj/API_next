1411f4ef2b3059096bdae36c6dc0f58f857a2af4
ronaldo araujo <ronaldoaraujo@jet.com.br>
2023-08-30 20:12:01 -0300

    implementando paginas estaticas

---
+++ pages/produtos/[id].js
@@ -0,0 +1,35 @@
+
+export async function getStaticPaths() {
+    return{
+        paths:[{
+            params:
+            {
+                id: '1'
+            }
+            
+        },{
+            params:
+            {
+                id: '2'
+            }
+        }],
+        fallback: 'blocking'
+    }
+}
+
+export async function getStaticProps(context) {
+    const id = context.params.id;
+
+    return{
+        props:{
+            id: id
+        }
+    }
+}
+
+function Produtos(props) {
+    return <div>Id do produto: {props.id}</div>
+    
+}
+
+export default Produtos;
