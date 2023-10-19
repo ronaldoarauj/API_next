ebe8573a952849afa77c543db203db72e82ab0d7
ronaldoarauj <ronaldoarauj@gmail.com>
2023-10-19 19:45:19 -0300

    teste

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ function Home(){
     return (
     <div>
         <h1>Home</h1>
-        <Link href="/sobre2"><a>Acessar página Sobre</a></Link>    
+        <Link href="/sobre"><a>Acessar página Sobre</a></Link>    
     </div>
     )
     
