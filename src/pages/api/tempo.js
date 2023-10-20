cdb037fa57677685eff8908f2fa78f08e6e28248
ronaldoarauj <ronaldoarauj@gmail.com>
2023-10-20 20:23:45 -0300

    teste2

--- src/pages/api/tempo.js
+++ src/pages/api/tempo.js
@@ -1,18 +1,18 @@
-async function Tempo(request, response) {
-    const apiSecret = process.env.SECRET_API_KEY;
-    const dynamicDate = new Date();
+// async function Tempo(request, response) {
+//     const apiSecret = process.env.SECRET_API_KEY;
+//     const dynamicDate = new Date();
 
-    const stockResponse = await fetch(`https://brapi.dev/api/quote/ciel3`);
-    const stockResponseJson = await stockResponse.json();
-    const valor = stockResponseJson.results[0].regularMarketPrice;
+//     const stockResponse = await fetch(`https://brapi.dev/api/quote/ciel3`);
+//     const stockResponseJson = await stockResponse.json();
+//     const valor = stockResponseJson.results[0].regularMarketPrice;
 
-    response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+//     response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
 
-    response.json({
-        date: dynamicDate.toGMTString(),
-        valor: valor
-    });
-}
+//     response.json({
+//         date: dynamicDate.toGMTString(),
+//         valor: valor
+//     });
+// }
 
 
-export default Tempo;
+// export default Tempo;
