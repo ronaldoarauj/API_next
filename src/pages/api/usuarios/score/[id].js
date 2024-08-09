ea0f01797cee12b4f3fb4faf595524e9607338ba
ronaldoarauj <ronaldoarauj@gmail.com>
2024-08-09 12:56:19 -0300

    ajuste listar

--- src/pages/api/usuarios/score/[id].js
+++ src/pages/api/usuarios/score/[id].js
@@ -65,7 +65,7 @@ export default async function handler(req, res) {
                             });
                         } else {
                             var score = await query({
-                                query: "SELECT  * FROM grace_user ORDER BY s_score DESC, id DESC LIMIT 3",
+                                query: "SELECT  * FROM grace_user ORDER BY s_score DESC, id DESC LIMIT 10",
                             });
                         }
 
